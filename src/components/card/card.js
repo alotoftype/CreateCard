89b78f7f7ccdd6a753086b5170ca9dec21836b4b
smjr <alotoftype@gmail.com>
2019-05-27 12:54:27 -0400

    Creating Card Component
    Adding Card Component to the project

---
+++ src/components/card/card.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+export default class Card extends Component {
+    constructor(){
+        super(props)
+        state: {
+
+        }
+    }
+
+    render(){
+        
+    return (
+        <div className="card">
+            <p>This is the card</p>
+        </div>
+    )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,50 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./card/styles.css";
+import Card from './components/card/card';
+import "./components/card/styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Card Challenge</h1>
-      <p>
-        Code this component with a stylesheet in whatever language you choose.
-      </p>
-      <p>
-        <a
-          target="_blank"
-          href="https://projects.invisionapp.com/share/FGR3VWU45YH#/screens/353365707_Show_Card"
-        >
-          Follow this mockup for design, color and states
-        </a>
-        .Other assets are:{" "}
-        <a
-          target="_blank"
-          href="https://cdn.glitch.com/bf244f65-c514-428a-b4b2-ac2746281e91%2Fchevron.svg?1555596324053"
-        >
-          chevron
-        </a>
-        ,{" "}
-        <a
-          target="_blank"
-          href="https://cdn.glitch.com/bf244f65-c514-428a-b4b2-ac2746281e91%2Finfo.svg?1555596324183"
-        >
-          info.svg
-        </a>
-        , and a{" "}
-        <a
-          target="_blank"
-          href="https://cdn.glitch.com/bf244f65-c514-428a-b4b2-ac2746281e91%2Fposter.jpg?1555596324072"
-        >
-          show poster
-        </a>
-        .
-      </p>
-      <p>
-        The More Performances link should toggle the rest of the card with an
-        animation of your choosing.
-      </p>
-      <p>Place the final result in the src/card directory.</p>
+    <Card />
     </div>
   );
 }
